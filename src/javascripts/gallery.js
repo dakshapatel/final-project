9e103d6874916331226a3274052598531a8cb884
Daksha <dakshapatel100@gmail.com>
2020-04-17 23:29:25 -0600

    final submission

---
+++ src/javascripts/gallery.js
@@ -0,0 +1,94 @@
+export const gallery = [
+  {
+    id: 1,
+    url:
+      "https://caputos.com/wp-content/uploads/Amedei-Acero-95-Front-350x435.jpg",
+    title: "Amedei Acero 95%",
+    description:
+      "Amedei challenges its philosophy and wisdom by creating a 95% dark chocolate with a strong and sweet taste."
+  },
+  {
+    id: 2,
+    url:
+      "https://caputos.com/wp-content/uploads/LetterPress-Ghana-Ashanti-100-web-350x435.jpg",
+    title: "Letterpress Ashanti, Ghana 100%",
+    description:
+      "With only 1 ingredient, this is the purest form of Ashanti Ghana they've ever made."
+  },
+  {
+    id: "3",
+    url:
+      "https://caputos.com/wp-content/uploads/Letterpress-Esmeraldas-Ecuador-100-350x435.jpg",
+    title: "Letterpress Costa Esmeraldas, Ecuador 100%",
+    description:
+      "With only 1 ingredient, this is the purest expression of Costa Esmeraldas they've ever made."
+  },
+  {
+    id: "4",
+    url: "https://caputos.com/wp-content/uploads/Marou-100-350x435.jpg",
+    title: "MAROU 100% DARK SINGLE ORIGIN VIETNAMESE CHOCOLATE BAR",
+    description:
+      "Discover an uncompromisingly dark chocolate, made from a select blend of the mildest cacao grown by farmers in the south of Vietnam."
+  },
+  {
+    id: "5",
+    url: "https://caputos.com/wp-content/uploads/Pump-Street-Ecuador-100-1.jpg",
+    title: "PUMP STREET ECUADOR HACIENDA LIMON 100%",
+    description:
+      "Made from pure Heirloom Cacao Preservation accredited cocoa beans on the Guantupi River in Ecuador"
+  },
+  {
+    id: "6",
+    url:
+      "https://caputos.com/wp-content/uploads/Ritual-Chocolate-One-Hundred-Percent-100-350x435.jpg",
+    title: "RITUAL CHOCOLATE 100% CACAO BLEND",
+    description:
+      "With only one ingredient and free of sugar, this is our chocolate in it's purest form. "
+  },
+  {
+    id: "7",
+    url:
+      "https://caputos.com/wp-content/uploads/Solstice-Bolivia-70-Front-350x435.jpg",
+    title: "SOLSTICE BOLIVIA CHOCOLATE BAR 70%",
+    description:
+      "Mature, full flavored, single origin bars that have stolen a piece of our hearts."
+  },
+  {
+    id: "8",
+    url:
+      "https://caputos.com/wp-content/uploads/Dick-Taylor-NORTHERNER-Blend-bar-768x1144.jpg",
+    title: "DICK TAYLOR NORTHERNER BLEND 73%",
+    description:
+      "This bar, named after the SS Northerner, hopes to capture the rugged and bold nature of the early ships to work these waters."
+  },
+  {
+    id: "9",
+    url:
+      "https://caputos.com/wp-content/uploads/Ritual-Chocolate-Ecuador-Camino-Verde-75.jpg",
+    title: "RITUAL CHOCOLATE ECUADOR 75%",
+    description:
+      "Ritual is quickly becoming America's best and brightest chocolate maker. Another masterpiece from this artisan American chocolate maker."
+  },
+  {
+    id: "10",
+    url: "https://caputos.com/wp-content/uploads/Solstice-India-70.jpg",
+    title: "SOLSTICE INDIA, ANAMALAI 70%",
+    description:
+      "Anamalai is a cacao, coconut, and nutmeg farm located in the southwestern tip of India. This chocolate has bright notes of passion fruit, subtle hints of peanut, and tangy raspberries."
+  },
+  {
+    id: "11",
+    url:
+      "https://caputos.com/wp-content/uploads/Mirzam-Tanzania-72-WB-350x435.jpg",
+    title: "MIRZAM TANZANIA DARK CHOCOLATE 72%",
+    description: "Notes: Red Berries, chocolatey and a slight touch of wood"
+  },
+  {
+    id: "12",
+    url:
+      "https://caputos.com/wp-content/uploads/Solstice-Vietnam-Ben-Tre-70.jpg",
+    title: "SOLSTICE VIETNAM, BEN TRE 70% (SPECIAL EDITION)",
+    description:
+      "Vietnam Ben Tre Chocolate is a special release chocolate bar that has beautiful flavors of honey, asian pear, raisin, and cinnamon"
+  }
+];
--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,7 +1,31 @@
 // Required by Webpack - do not touch
-require.context('../', true, /\.(html|json|txt|dat)$/i)
-require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
-require.context('../stylesheets/', true, /\.(css|scss)$/i)
+require.context("../", true, /\.(html|json|txt|dat)$/i);
+require.context("../images/", true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i);
+require.context("../stylesheets/", true, /\.(css|scss)$/i);
 
 // JavaScript
 //TODO
+import { gallery } from "./gallery";
+
+let featured_trail = document.querySelector(".card");
+for (let t of gallery) {
+  let trail_thumb = document.getElementById("t" + t.id);
+  trail_thumb.innerHTML = `
+    <img src="${t.url}">
+    <h5 class="card-title">${t.title}</h5>
+    <p class="card-text">${t.description}</p>
+
+    `;
+
+  trail_thumb.onclick = function() {
+    selectTrail(t);
+  };
+}
+
+// function selectTrail(t) {
+//   featured_trail.innerHTML = `
+//     <img src="${t.url}" style="float: left; margin-left:15px">
+//     <h2>${t.title}</h2>
+//     <p>${t.description}</p>
+//     `;
+// }
